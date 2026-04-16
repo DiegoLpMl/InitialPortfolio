309dd71476ca6c8718c73d1816fb7cb99986cb81
DiegoLopes <diegolopes13072007@gmail.com>
2026-04-16 15:15:13 -0300

    Backup

---
+++ src/main/java/org/com/example/javainterface/EsqueciSenhaController.java
@@ -0,0 +1,14 @@
+package org.com.example.javainterface;
+import javafx.animation.TranslateTransition;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.util.Duration;
+import java.io.IOException;
+import java.sql.SQLException;
+import org.mindrot.jbcrypt.BCrypt;
+
+public class EsqueciSenhaController {
+}
